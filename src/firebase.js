0094268c9b3ec6dfd738528369d3dcd581362181
Madoka Kato <maka7816@yahoo.co.jp>
2023-04-05 09:22:05 +0900

    typeの指定

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB8XoaoO2Oy-vGLfoJuELDGBGxToygDeNo",
-  authDomain: "attendance-system-595c9.firebaseapp.com",
-  projectId: "attendance-system-595c9",
-  storageBucket: "attendance-system-595c9.appspot.com",
-  messagingSenderId: "615030830711",
-  appId: "1:615030830711:web:921f65b09da82abaad3872",
-  measurementId: "G-2MMHEM01YL",
+  apiKey: "AIzaSyAW6qZQY1zUsjzWKXxxX072zHz2f3mmaZA",
+  authDomain: "attedance-systems2.firebaseapp.com",
+  projectId: "attedance-systems2",
+  storageBucket: "attedance-systems2.appspot.com",
+  messagingSenderId: "459819591613",
+  appId: "1:459819591613:web:9b2a0600beb7534bd5d4d4",
+  measurementId: "G-PMCSY1MW1V"
 };
 
 // Initialize Firebase
--- src/pages/member-attendance-log.tsx
+++ src/pages/member-attendance-log.tsx
@@ -1,18 +1,21 @@
 import Layout from "@/components/Layout";
-import useSWR from "swr";
-import {
-  getDocs,
-  collection,
-  where,
-  query,
-} from "firebase/firestore";
+
+import { getDocs, collection, where, query } from "firebase/firestore";
 import { db } from "../firebase";
 import { useRef, useState } from "react";
-import { fetchUsers } from "@/types/fetchUsers";
 import { fetchmemberattendance } from "@/types/member-attendance";
 
-export const getServerSideProps = async (id: { query: { id: number; }; }) => {
-  const user:fetchmemberattendance[]  = [];
+
+
+type tofetchmemberattendance = {
+  enterTime?: any;
+  exitTime?: any;
+  id?: number;
+  date:any;
+  week:string
+};
+export const getServerSideProps = async (id: { query: { id: number } }) => {
+  const user: fetchmemberattendance[] = [];
   const useRef = collection(db, "users-attendance");
   const q = query(useRef, where("id", "==", `${id.query.id}`));
   const querySnapshot = await getDocs(q);
@@ -27,30 +30,26 @@ export const getServerSideProps = async (id: { query: { id: number; }; }) => {
 
   return {
     props: {
-      //javascriptオブジェクト
+      //javascriptオブジェクト(タイムスタンプをシリアライズする必要がある)
       data: JSON.stringify(user),
     },
   };
 };
 
-export default function MemberAttendanceLog({ data }) {
+export default function MemberAttendanceLog({ data }: { data: string }) {
   const [month, setMonth] = useState(new Date().getMonth() + 1);
   const [year, setYear] = useState(new Date().getFullYear());
-
   if (!data) return;
   const datam = JSON.parse(data);
   //取得したデータに新たなオブジェクトdateを追加して00/00の形で保存
-  datam.map((t) => {
+  datam.map((t:tofetchmemberattendance) => {
     t.date = `${new Date(t.enterTime).getMonth() + 1}/${new Date(
       t.enterTime
     ).getDate()}`;
   });
 
   //最終日計算
-  let lastday = new Date(year, month, 0);
-  //最終日の日付のみ
-  lastday = lastday.getDate();
-
+  let lastday = new Date(year, month, 0).getDate();
 
   let date = [];
   for (var i = 1; i <= lastday; i++) {
@@ -61,8 +60,7 @@ export default function MemberAttendanceLog({ data }) {
     date.push({ day: `${month}/${i}`, date: i, weeks: weeks });
   }
 
-
-  let datefinish = [];
+  let datefinish:tofetchmemberattendance[] = [];
   date.map((d) => {
     const filterdam = datam.filter((dm) => {
       return dm.date === d.day;
@@ -88,12 +86,12 @@ export default function MemberAttendanceLog({ data }) {
   //プルダウン用：1月〜12月
   let pulldownmonth = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-  const changeYear = (e) => {
+  const changeYear = (e: React.ChangeEvent<HTMLInputElement>) => {
     setYear(e.target.value);
   };
 
-  const changeMonth = (e) => {
-    console.log(e.target.value);
+  const changeMonth = (e: React.ChangeEvent<HTMLInputElement>) => {
+
     setMonth(e.target.value);
   };
 
@@ -147,14 +145,14 @@ export default function MemberAttendanceLog({ data }) {
                   {d.enterTime === ""
                     ? ""
                     : `${
-                      new Date(d.enterTime).getMinutes() < 10
-                        ? `${new Date(d.enterTime).getHours()}:0${new Date(
-                            d.enterTime
-                          ).getMinutes()}`
-                        : `${new Date(d.enterTime).getHours()}:${new Date(
-                            d.enterTime
-                          ).getMinutes()}`
-                    }`}
+                        new Date(d.enterTime).getMinutes() < 10
+                          ? `${new Date(d.enterTime).getHours()}:0${new Date(
+                              d.enterTime
+                            ).getMinutes()}`
+                          : `${new Date(d.enterTime).getHours()}:${new Date(
+                              d.enterTime
+                            ).getMinutes()}`
+                      }`}
                 </td>
                 <td>
                   {d.exitTime === ""
