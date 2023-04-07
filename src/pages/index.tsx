bc572646e13310e4a94d785b4c01045fc13b2610
konishi <tatsuki.konishi@rakus-partners.co.jp>
2023-04-07 14:03:58 +0900

    indexファイルのテスト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import index from "../style/index.module.scss";
 import axios from "axios";
 import Router from "next/router";
 import { Users } from "../types/user";
-import { User } from "firebase/auth";
 
 //入場
 const Index = () => {
@@ -20,11 +19,13 @@ const Index = () => {
         const time = Time();
         const nowTime = `${time.year}/${time.mon}/${time.day}`;
         const nowEntries = response.data;
+        console.log(nowEntries);
         //現在入場者情報取得
         const nowEntryUsersItem: Users[] = nowEntries.filter(
           (nowEntry: Users) => {
             if (nowEntry.enterTime) {
               const entrydate = new Date(nowEntry.enterTime);
+              console.log(entrydate);
               const entryYear = entrydate.getFullYear();
               const entryMonth = entrydate.getMonth();
               const entryDate = entrydate.getDate();
@@ -56,21 +57,26 @@ const Index = () => {
               </tr>
             </thead>
             <tbody>
-              {nowEntryUsers.map((item: Users, index) => (
-                <tr key={index}>
-                  <td>{item.name}</td>
-
-                  {new Date(item.enterTime).getMinutes() < 10 ? (
-                    <td>{`${new Date(item.enterTime).getHours()}:0${new Date(
-                      item.enterTime
-                    ).getMinutes()}`}</td>
-                  ) : (
-                    <td>{`${new Date(item.enterTime).getHours()}:${new Date(
-                      item.enterTime
-                    ).getMinutes()}`}</td>
-                  )}
-                </tr>
-              ))}
+              {nowEntryUsers
+                ? nowEntryUsers.map((item: Users, index) => (
+                    <tr key={index} data-testid="character">
+                      <td data-testid="name">{item.name}</td>
+                      {new Date(item.enterTime).getMinutes() < 10 ? (
+                        <td>
+                          {`${new Date(item.enterTime).getHours()}:0${new Date(
+                            item.enterTime
+                          ).getMinutes()}`}
+                        </td>
+                      ) : (
+                        <td>
+                          {`${new Date(item.enterTime).getHours()}:${new Date(
+                            item.enterTime
+                          ).getMinutes()}`}
+                        </td>
+                      )}
+                    </tr>
+                  ))
+                : null}
             </tbody>
           </table>
         </div>
@@ -84,31 +90,37 @@ const Index = () => {
               </tr>
             </thead>
             <tbody>
-              {nowEntryUsers.map((item: Users, index) => (
-                <tr key={index}>
-                  {item.exitTime !== "NaN" &&
-                  item.exitTime.split("T")[0] ===
-                    new Date().toISOString().split("T")[0] ? (
-                    new Date(item.exitTime).getMinutes() < 10 ? (
-                      <>
-                        <td>{item.name}</td>
-                        <td>{`${new Date(item.exitTime).getHours()}:0${new Date(
-                          item.exitTime
-                        ).getMinutes()}`}</td>
-                      </>
-                    ) : (
-                      <>
-                        <td>{item.name}</td>
-                        <td>{`${new Date(item.exitTime).getHours()}:${new Date(
-                          item.exitTime
-                        ).getMinutes()}`}</td>
-                      </>
-                    )
-                  ) : (
-                    <td></td>
-                  )}
-                </tr>
-              ))}
+              {nowEntryUsers
+                ? nowEntryUsers.map((item: Users, index) => (
+                    <tr key={index}>
+                      {item.exitTime !== "NaN" &&
+                      item.exitTime.split("T")[0] ===
+                        new Date().toISOString().split("T")[0] ? (
+                        new Date(item.exitTime).getMinutes() < 10 ? (
+                          <>
+                            <td>{item.name}</td>
+                            <td>{`${new Date(
+                              item.exitTime
+                            ).getHours()}:0${new Date(
+                              item.exitTime
+                            ).getMinutes()}`}</td>
+                          </>
+                        ) : (
+                          <>
+                            <td>{item.name}</td>
+                            <td>{`${new Date(
+                              item.exitTime
+                            ).getHours()}:${new Date(
+                              item.exitTime
+                            ).getMinutes()}`}</td>
+                          </>
+                        )
+                      ) : (
+                        <td></td>
+                      )}
+                    </tr>
+                  ))
+                : null}
             </tbody>
           </table>
         </div>
