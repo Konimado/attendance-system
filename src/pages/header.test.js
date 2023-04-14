05e73016627b68b7338c8b2d0261763d2eaca4ac
konishi <tatsuki.konishi@rakus-partners.co.jp>
2023-04-14 11:41:41 +0900

    画面遷移テスト(ヘッダー)

--- src/pages/header.test.js
+++ src/pages/header.test.js
@@ -1,23 +1,13 @@
-/**
- * @jest-environment jsdom
- */
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import userEvent from "@testing-library/user-event";
-import { getPage } from "next-page-tester";
-import { initTestHelpers } from "next-page-tester";
-// import "setimmediate";
+import Header from "../components/header";
+import { render } from "@testing-library/react";
+import { auth } from "../firebase";
+import { useAuthState } from "../__mocks__/react-firebase-hooks/auth";
 
-initTestHelpers();
-
-describe("Navigation by Link", () => {
-  it("Should route to selected page in navbar", async () => {
-    const { page } = await getPage({
-      route: "/",
-    });
-    render(page);
-
-    userEvent.click(screen.getByTestId("create-user-nav"));
-    expect(await screen.findByText("会員登録")).toBeInTheDocument();
-  });
+it("should be mocked", () => {
+  // Change the implementation of the mock
+  useAuthState.mockReturnValue([false]);
+  const { getByText } = render(<Header />);
+  // Expect that the app file got the mocked value
+  expect(useAuthState).toBeCalledWith(auth);
+  expect(getByText("ログイン")).toBeTruthy();
 });
