53e0388df8449b6f0ea09a2fd418be27b143f5e3
konishi <tatsuki.konishi@rakus-partners.co.jp>
2023-04-18 14:19:05 +0900

    member-listテスト

--- src/pages/member-list.test.js
+++ src/pages/member-list.test.js
@@ -1,62 +1,61 @@
 /**
  * @jest-environment jsdom
  */
-import "@testing-library/jest-dom/extend-expect";
-import { render, screen, cleanup } from "@testing-library/react";
-import { getPage } from "next-page-tester";
+
 import { initTestHelpers } from "next-page-tester";
-import { rest } from "msw";
-import { SetupServer, setupServer } from "msw/lib/node";
-import MemberList from "../pages/member-list";
+import { render, screen } from "@testing-library/react";
+import MemberList, { getStaticProps } from "./member-list";
 
 initTestHelpers();
 
-const headlers = [
-  res.get("/api/user_get", (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          address: "aaa",
-          birth: "",
-          enterTime: "1",
-          exitTime: "1",
-          gender: "",
-          id: "",
-          mailAddress: "",
-          name: "",
-          phoneNumber: "",
-          plan: "",
-          postalCode: "",
-          startDate: "",
-          statue: "1",
-        },
-      ])
-    );
-  }),
+const res = [
+  {
+    address: "神奈川県横須賀市西浦賀",
+    birth: "2002/02/05",
+    enterTime: { seconds: 1681725899, nanoseconds: 495000000 },
+    exitTime: { seconds: 1681725901, nanoseconds: 399000000 },
+    gender: "male",
+    id: "2349",
+    mailAddress: "matsu@gmail.com",
+    name: "松川 一静",
+    phoneNumber: "070-2222-2222",
+    plan: "daily",
+    postalCode: "239-0824",
+    startDate: "2012/02/02",
+    statue: false,
+  },
+  {
+    address: "宮城県石巻市鮎川浜",
+    birth: "1996/06/21",
+    enterTime: { seconds: 1681725909, nanoseconds: 814000000 },
+    exitTime: { seconds: 1681725911, nanoseconds: 259000000 },
+    gender: "男性",
+    id: 2909,
+    mailAddress: "hinata@yahoo.co.jp",
+    name: "日向 翔陽",
+    phoneNumber: "090-3333-2222",
+    plan: "all",
+    postalCode: "986-2523",
+    startDate: "2002/03/02",
+    statue: false,
+  },
 ];
-const server = setupServer(...headlers);
 
-beforeAll(() => {
-  server.listen();
-});
-afterEach(() => {
-  server.restoreHandlers();
-  cleanup();
-});
-afterAll(() => {
-  server.close();
+jest.mock("./api/member_list_get", () => {
+  const originalModule = jest.requireActual("./api/member_list_get");
+  return {
+    __esModule: false,
+    ...originalModule,
+    getMemberList: jest.fn(() => res),
+  };
 });
 
-describe("member-list page", () => {
-  it("Should render the list of member-list by getStaticprops", async () => {
-    const { page } = await getPage({
-      route: "/member-list",
-    });
-    render(page)
-    expect(await screen.findAllByText('')).toBeInTheDocument()
-    expect(screen.getByText('')).toBeInTheDocument()
-    expect(screen.getByText('')).toBeInTheDocument()
-    screen.debug();
-  });
+it("getserversideprops ", async () => {
+  const { props } = await getStaticProps();
+  expect(props.users).toEqual(JSON.stringify(res));
+}, 20000);
+
+it("render time", () => {
+  render(<MemberList users={JSON.stringify(res)} />);
+  expect(screen.getByText("松川 一静")).toBeInTheDocument();
 });
